958fb103f9259c5bf7907b3bdade5e15dbe5c2b4
John <johnooceans@gmail.com>
2026-05-10 18:22:19 +0200

    Pedir notas

--- src/mainRefactorizada.java
+++ src/mainRefactorizada.java
@@ -2,7 +2,30 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class mainRefactorizada {
+public class MainRefactorizada {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
 
+        Map<String, Double> notas = new HashMap<>();
 
+        //Pedir Asignaturas
+        boolean continuar = true;
+        while (continuar) {
+
+             System.out.println("Asignatura: ");
+             String asignatura = scan.nextLine();
+
+             if (asignatura.equalsIgnoreCase("fin")) {
+
+                continuar = false;    
+
+             } else {
+
+                System.out.println("Nota: ");
+                double nota= scan.nextDouble();
+                scan.nextLine();
+
+                notas.put(asignatura, nota);
+             }
+    }
 }
