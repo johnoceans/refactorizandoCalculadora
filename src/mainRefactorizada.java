bea0ccba6eeecf457d3f43d7dc328d9b4709c572
John <johnooceans@gmail.com>
2026-05-11 19:39:21 +0200

    Implementando métodos

--- src/mainRefactorizada.java
+++ src/mainRefactorizada.java
@@ -26,126 +26,26 @@ public class MainRefactorizada {
 
             switch (opcion) {
                 case 1:
-                    // Pedir Asignaturas
-                    System.out.println(
-                            "A continuacion te pediremos que introduzcas tu asignatura y seguido la nota. \n Cuando hayas terminado, escribe la palabra 'fin' para guardar.");
-
-                    boolean continuar = true;
-
-                    while (continuar) {
-
-                        System.out.println("Asignatura: ");
-                        String asignatura = scan.nextLine();
-
-                        if (asignatura.equalsIgnoreCase("fin")) {
-
-                            continuar = false;
-
-                        } else {
-
-                            System.out.println("Nota: ");
-                            double nota = scan.nextDouble();
-                            scan.nextLine();
-
-                            notas.put(asignatura, nota);
-                        }
-                    }
+                    agregarAsignaturas(notas, scan);
                     break;
 
                 case 2:
-                    System.out.println("\n--- LISTADO DE NOTAS ---");
-                    for (String asignatura : notas.keySet()) {
-                        System.out.println(asignatura + " -> " + notas.get(asignatura));
-                    }
+                    mostrarNotas(notas);
                     break;
 
                 case 3:
-                    // Modificar Asignatura
-                    System.out.println("\n¿Deseas modificar alguna nota? \n 1. Sí \n 2. No");
-
-                    int opcionModificar = scan.nextInt();
-                    scan.nextLine();
-
-                    while (opcionModificar == 1) {
-
-                        System.out.println("Asignatura a modificar: ");
-                        String asignaturaModificar = scan.nextLine();
-
-                        if (notas.containsKey(asignaturaModificar)) {
-
-                            System.out.println("Nueva nota: ");
-                            double nuevaNota = scan.nextDouble();
-                            scan.nextLine();
-
-                            notas.put(asignaturaModificar, nuevaNota);
-
-                            System.out.println("Nota modificada correctamente.");
-                        } else {
-                            System.out.println("La asignatura no existe.");
-                        }
-
-                        System.out.println("\n¿Deseas modificar otra nota? \n 1. Sí \n 2. No");
-
-                        opcionModificar = scan.nextInt();
-                        scan.nextLine();
-                    }
+                    modificarNota(notas, scan);
                     break;
 
                 case 4:
-                    // Eliminar nota
-                    System.out.println("\n--- LISTADO DE NOTAS ---");
-                    for (String asignatura : notas.keySet()) {
-                        System.out.println(asignatura + " -> " + notas.get(asignatura));
-                    }
-
-                    System.out.println("\n¿Deseas Eliminar alguna nota? \n 1. Sí \n 2. No");
-
-                    int opcionEliminar = scan.nextInt();
-                    scan.nextLine();
-
-                    while (opcionEliminar == 1) {
-
-                        System.out.println("Asignatura a eliminar: ");
-                        String asignaturaEliminar = scan.nextLine();
-
-                        if (notas.containsKey(asignaturaEliminar)) {
-
-                            notas.remove(asignaturaEliminar);
-
-                            System.out.println("Asignatura eliminada correctamente.");
-                        } else {
-                            System.out.println("La asignatura no existe.");
-                        }
-
-                        if (!notas.isEmpty()) {
-                            System.out.println("\n--- LISTADO DE NOTAS ---");
-                            for (String asignatura : notas.keySet()) {
-                                System.out.println(asignatura + " -> " + notas.get(asignatura));
-                            }
-                            System.out.println("\n¿Deseas eliminar otra nota? \n 1. Sí \n 2. No");
-
-                            opcionEliminar = scan.nextInt();
-                            scan.nextLine();
-                        } else {
-                            System.out.println("No quedan asignaturas.");
-
-                            opcionEliminar = 2;
-                        }
-
-                    }
+                    eliminarAsignatura(notas, scan);
                     break;
 
                 case 5:
                     // Calcular media
-                    double suma = 0;
-
-                    for (Double nota : notas.values()) {
-                        suma += nota;
-                    }
-
                     if (!notas.isEmpty()) {
 
-                        double media = suma / notas.size();
+                        double media = calcularMedia(notas);
                         System.out.println("\nLa media final es: " + media);
 
                     } else {
@@ -164,5 +64,117 @@ public class MainRefactorizada {
 
         }
         scan.close();
+    }
+
+    public static void mostrarNotas(Map<String, Double> notas) {
+        System.out.println("\n--- LISTADO DE NOTAS ---");
+        for (String asignatura : notas.keySet()) {
+            System.out.println(asignatura + " -> " + notas.get(asignatura));
+        }
+    }
+
+    public static double calcularMedia(Map<String, Double> notas) {
+        double suma = 0;
+
+        for (Double nota : notas.values()) {
+            suma += nota;
+        }
+
+        return suma / notas.size();
+    }
+
+    public static void agregarAsignaturas(Map<String, Double> notas, Scanner scan) {
+        // Pedir Asignaturas
+        System.out.println(
+                "A continuacion te pediremos que introduzcas tu asignatura y seguido la nota. \n Cuando hayas terminado, escribe la palabra 'fin' para guardar.");
+
+        boolean continuar = true;
+
+        while (continuar) {
+
+            System.out.println("Asignatura: ");
+            String asignatura = scan.nextLine();
+
+            if (asignatura.equalsIgnoreCase("fin")) {
+
+                continuar = false;
+
+            } else {
+
+                System.out.println("Nota: ");
+                double nota = scan.nextDouble();
+                scan.nextLine();
+
+                notas.put(asignatura, nota);
+            }
+        }
+    }
+
+    public static void modificarNota(Map<String, Double> notas, Scanner scan) {
+        // Modificar Asignatura
+        System.out.println("\n¿Deseas modificar alguna nota? \n 1. Sí \n 2. No");
+
+        int opcionModificar = scan.nextInt();
+        scan.nextLine();
+        if (opcionModificar == 1) {
+
+            while (opcionModificar == 1) {
+
+                System.out.println("Asignatura a modificar: ");
+                String asignaturaModificar = scan.nextLine();
+
+                if (notas.containsKey(asignaturaModificar)) {
+
+                    System.out.println("Nueva nota: ");
+                    double nuevaNota = scan.nextDouble();
+                    scan.nextLine();
+
+                    notas.put(asignaturaModificar, nuevaNota);
+
+                    System.out.println("Nota modificada correctamente.");
+                } else {
+                    System.out.println("La asignatura no existe.");
+                }
+
+                System.out.println("\n¿Deseas modificar otra nota? \n 1. Sí \n 2. No");
+
+                opcionModificar = scan.nextInt();
+                scan.nextLine();
+            }
+        }
+    }
+
+    public static void eliminarAsignatura(Map<String, Double> notas, Scanner scan) {
+        // Eliminar nota
+        mostrarNotas(notas);
+
+        System.out.println("\n¿Deseas Eliminar alguna nota? \n 1. Sí \n 2. No");
+
+        int opcionEliminar = scan.nextInt();
+        scan.nextLine();
+
+        while (opcionEliminar == 1 && !notas.isEmpty()) {
+
+            System.out.println("Asignatura a eliminar: ");
+            String asignaturaEliminar = scan.nextLine();
+
+            if (notas.containsKey(asignaturaEliminar)) {
+
+                notas.remove(asignaturaEliminar);
+
+                System.out.println("Asignatura eliminada correctamente.");
+            } else {
+                System.out.println("La asignatura no existe.");
+            }
+
+            if (!notas.isEmpty()) {
+                mostrarNotas(notas);
+
+                System.out.println("\n¿Deseas eliminar otra nota? \n 1. Sí \n 2. No");
+
+                opcionEliminar = scan.nextInt();
+                scan.nextLine();
+            }
+        }
     }
 }
